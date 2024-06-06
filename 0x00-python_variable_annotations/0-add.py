7ca0e5f8de01e61ef56d24f9a5ad2caf1be64df9
ukaemma2 <ukaemmanuel2000@gmail.com>
2024-06-06 21:29:36 +0000

    errs_fixed

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,5 +2,5 @@
 """Module that contains a single function that adds two integers."""
 
 def add(a: float, b: float) -> float:
-    """ Add two integer together """
+    """ Add two integers together """
     return a + b
